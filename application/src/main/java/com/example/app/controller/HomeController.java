4765b87a954843a4e262d611a67e6f139f80002f
krios2146 <daun2146@yandex.ru>
2022-04-29 12:50:07 +0700

    Migrate from mustache to freemarker (part 1/2)

--- application/src/main/java/com/example/app/controller/HomeController.java
+++ application/src/main/java/com/example/app/controller/HomeController.java
@@ -2,8 +2,6 @@ package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.Map;
 
 @Controller
